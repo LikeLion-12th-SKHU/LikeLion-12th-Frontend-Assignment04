f1b6fa8ab53f7b36a7f12192ec646a79b3f7d43d
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-05-01 14:53:03 +0900

    Feat:all done

---
+++ YeeunPark/index.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('.form')
+const input = document.querySelector('.input')
+const todos = document.querySelector('.todos')
+const numsDisplay = document.querySelector('.nums')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTodo()
+})
+
+const completedNums = () => {
+    const completedTodos = document.querySelectorAll('.completed').length
+    const totalTodos = todos.children.length
+    numsDisplay.innerHTML = `${completedTodos} / ${totalTodos}`
+}
+
+const addTodo = (todo) => {
+    let todoInput = input.value
+
+    if (todoInput === '') {
+        alert("최소 한 글자 이상 입력하세요!");
+        return;
+    }
+
+    const $todoElement = document.createElement('li')
+
+    $todoElement.innerHTML = todoInput
+    $todoElement.addEventListener('click', () => {
+        $todoElement.classList.toggle('completed')
+        completedNums()
+    })
+
+    $todoElement.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
+        $todoElement.remove()
+        completedNums()
+    })
+
+    todos.append($todoElement)
+    input.value = ''
+    completedNums()
+}
